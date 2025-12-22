f5f247d3bb7fd8a3cf9bcd4decd170c8c2d645a4
Vansh Deep Singh <vs493599@gmail.com>
2025-12-22 13:35:36 +0530

    Repo created

---
+++ src/main/java/com/example/schedularservice/SchedularServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedularservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedularServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedularServiceApplication.class, args);
+    }
+
+}
